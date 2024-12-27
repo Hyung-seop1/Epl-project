668b92762b135a53ac286e69c8505feb000c5e07
Hyungseop <kgkfdg@gmail.com>
2024-12-26 20:19:55 -0500

    Adding kick off time

--- EPL-Project/pages/Matches.jsx
+++ EPL-Project/pages/Matches.jsx
@@ -13,17 +13,19 @@ export default function Matches() {
 
     return (
         <>
-            <div>
+            <div className="matchTable">
                 {Object.entries(groupedData).map(([event, matches]) => (
                     <div key={event}>
                         <h2>Matchday {event} of 38</h2>
                         {matches.map((match, index) => (
-                            <div key={index}>
-                                <strong>Team A:</strong> {match.team_a}
-                                <br />
-                                <strong>Team H:</strong> {match.team_h}
-                                <br />
-                                <hr />
+                            <div key={index} className="eachEvent">
+                                <div className="teamInfo">
+                                    Team A: {match.team_a} {match.team_a_score}
+                                    <br />
+                                    Team H: {match.team_h} {match.team_h_score}
+                                    <br />
+                                </div>
+                                <div>{match.kickoff_time}</div>
                             </div>
                         ))}
                     </div>
