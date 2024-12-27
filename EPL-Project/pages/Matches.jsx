cf506bbdf947ef8b0de849a274aa80a2c19af526
Hyungseop <kgkfdg@gmail.com>
2024-12-26 20:58:00 -0500

    Adding arrow for moving previous or next matches

--- EPL-Project/pages/Matches.jsx
+++ EPL-Project/pages/Matches.jsx
@@ -3,6 +3,8 @@ import fixtureData from "../data/fixture.json";
 import informationData from "../data/information.json";
 
 export default function Matches() {
+    const [currentEvent, setCurrentEvent] = useState(1);
+    const totalEvents = 38;
     const groupedData = fixtureData.reduce((acc, match) => {
         if (!acc[match.event]) {
             acc[match.event] = [];
@@ -11,27 +13,50 @@ export default function Matches() {
         return acc;
     }, {});
 
+    const handlePrevEvent = () => {
+        if (currentEvent > 1) {
+            setCurrentEvent(currentEvent - 1);
+        }
+    };
+
+    const handleNextEvent = () => {
+        if (currentEvent < totalEvents) {
+            setCurrentEvent(currentEvent + 1);
+        }
+    };
     return (
         <>
             <div className="matchTable">
-                {Object.entries(groupedData).map(([event, matches]) => (
-                    <div key={event}>
-                        <h2>Matchday {event} of 38</h2>
-                        {matches.map((match, index) => (
-                            <div key={index} className="eachEvent">
-                                <div className="teamInfo">
-                                    Team A: {match.team_a} {match.team_a_score}
-                                    <br />
-                                    Team H: {match.team_h} {match.team_h_score}
-                                    <br />
-                                </div>
-                                <div>{match.kickoff_time}</div>
-                            </div>
-                        ))}
+                <h2 className="mathTitle">
+                    <div className="prev">
+                        <i
+                            className="bx bxs-left-arrow"
+                            onClick={handlePrevEvent}
+                        ></i>
+                    </div>
+                    <div>
+                        Matchday {currentEvent} of {totalEvents}
+                    </div>
+
+                    <div className="next">
+                        <i
+                            className="bx bxs-right-arrow"
+                            onClick={handleNextEvent}
+                        ></i>
+                    </div>
+                </h2>
+                {groupedData[currentEvent]?.map((match, index) => (
+                    <div key={index} className="eachEvent">
+                        <div className="teamInfo">
+                            Team A: {match.team_a} {match.team_a_score}
+                            <br />
+                            Team H: {match.team_h} {match.team_h_score}
+                            <br />
+                        </div>
+                        <div className="matchTime">{match.kickoff_time}</div>
                     </div>
                 ))}
             </div>
-            ;
         </>
     );
 }
