fc3031fce05aa8acb6195926ee7aae1c40bfbd2d
Hyungseop <kgkfdg@gmail.com>
2025-01-02 00:35:35 -0500

    Add verification for email and password

--- EPL-Project/src/pages/LogIn.jsx
+++ EPL-Project/src/pages/LogIn.jsx
@@ -1,9 +1,13 @@
 import logos from "../component/getImg";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function logIn() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const navigate = useNavigate();
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -11,6 +15,33 @@ export default function logIn() {
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        let isValid = true;
+
+        // Verification for email
+        const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!email) {
+            setEmailError("Email is required.");
+            isValid = false;
+        } else if (!emailFormat.test(email)) {
+            setEmailError("Please enter a valid email address.");
+            isValid = false;
+        }
+
+        // Verification for password
+        if (!password) {
+            setPasswordError("Password is required.");
+        } else if (password.length < 6) {
+            setPasswordError("Password must be at least 6 characters long.");
+            isValid = false;
+        }
+
+        if (isValid) {
+            navigate("/");
+        }
     };
 
     return (
@@ -31,6 +62,7 @@ export default function logIn() {
                             value={email}
                             onChange={handleEmailChange}
                         />
+                        {emailError && <p className="error">{emailError}</p>}
                         <label htmlFor="password"> </label>
                         <input
                             type="password"
@@ -40,11 +72,14 @@ export default function logIn() {
                             value={password}
                             onChange={handlePasswordChange}
                         />
+                        {passwordError && (
+                            <p className="error">{passwordError}</p>
+                        )}
                     </div>
-                    <button>Login to your account</button>
+                    <button onClick={handleLogin}>Login to your account</button>
                     <div className="linkToSignUp">
                         <p>Don't have an account?</p>
-                        <p>Sign Up</p>
+                        <p className="signup">Sign Up</p>
                     </div>
                 </div>
             </div>
